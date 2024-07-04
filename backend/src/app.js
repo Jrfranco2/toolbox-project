d2cd3a2aeb29711ce3d93ce33800b5a2baa6f39c
Jonathan Franco <jonas150397@gmail.com>
2024-07-03 20:15:38 -0500

    [backend] installing testing dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,36 @@ app.get('/files/data', async (req, res) => {
 
     const files = filesResponse.data.files;
 
-    res.json(files);
+    const filePromises = files.map(async (file) => {
+      try {
+        const fileDataResponse = await axios.get(`${API_URL}/file/${file}`, {
+          headers: { Authorization: API_KEY },
+        });
+
+        const lines = fileDataResponse.data
+          .split('\n')
+          .slice(1)
+          .map((line) => line.split(','))
+          .filter(
+            (columns) =>
+              columns.length === 4 && columns.every((col) => col.trim() !== '')
+          )
+          .map((columns) => ({
+            text: columns[1],
+            number: parseInt(columns[2]),
+            hex: columns[3],
+          }));
+
+        return { file, lines };
+      } catch (error) {
+        console.error(`Error processing file: ${file}:`, error.message);
+        return { file, lines: [] };
+      }
+    });
+
+    const fileData = await Promise.allSettled(filePromises);
+
+    res.json(fileData.map((result) => result.value));
   } catch (error) {
     const statusCode = error.response ? error.response.status : 500;
     return res.status(statusCode).json({
